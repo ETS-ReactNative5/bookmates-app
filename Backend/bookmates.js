633909bdcfff9ec5e59a2a2e76b831eb80daf67b
Laurena Fayad <laurenafayad@gmail.com>
2022-03-12 08:39:47 +0200

    Added User model and initialized mongoose connection

--- Backend/bookmates.js
+++ Backend/bookmates.js
@@ -1,6 +1,37 @@
 //jshint esversion:6
-const express = require("express")
+const express = require("express");
+const cors = require("cors");
+var corsOptions = {
+    origin: "http://localhost:3000"
+};
+const jwt = require("jsonwebtoken");
 const app = express()
 
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+//Connect to db
+const db = require("./app/models");
+db.mongoose
+  .connect(`mongodb://${dbConfig.HOST}:${dbConfig.PORT}/${dbConfig.DB}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
+
+app.get("/", (req, res) => {
+    res.json({ message: "Welcome to bookmates application." });
+  });
 
 app.listen(8000);
