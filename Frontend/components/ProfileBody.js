cf856ebe9cd5cc1b9bc92cd74f83f99df74f0afc
Laurena Fayad <laurenafayad@gmail.com>
2022-03-20 14:45:23 +0200

    My Profile: Added Bio text prop to profile header

--- Frontend/components/ProfileBody.js
+++ Frontend/components/ProfileBody.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text, Image, TouchableOpacity} from 'react-native';
+import {View, Text, Image, TouchableOpacity, SafeAreaView, StatusBar} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
 export const ProfileBody = ({
   name,
@@ -9,39 +9,42 @@ export const ProfileBody = ({
   bio
 }) => {
   return (
-      <View
+      <SafeAreaView>
+        <View
         style={{
           flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'space-around',
+          justifyContent: 'space-between',
           paddingVertical: 20,
         }}>
-        <View>
-          <Image
-            source={profileImage}
-            style={{
-              resizeMode: 'cover',
-              width: 70,
-              height: 70,
-              borderRadius: 100,
-            }}
-          />
-          <Text
-            style={{
-              paddingVertical: 5,
-              fontWeight: 'bold',
-            }}>
-            {name}
-          </Text>
+            <View>
+                <Image
+                    source={profileImage}
+                    style={{
+                    resizeMode: 'cover',
+                    width: 70,
+                    height: 70,
+                    borderRadius: 100,
+                    }}
+                />
+                <Text
+                    style={{
+                    paddingVertical: 10,
+                    fontWeight: 'bold',
+                    }}>
+                    {name}
+                </Text>
+            </View>
+            <View style={{alignItems: 'center'}}>
+                <Text style={{fontWeight: 'bold', fontSize: 18}}>{followers}</Text>
+                <Text>Followers</Text>
+            </View>
+            <View style={{alignItems: 'center'}}>
+                <Text style={{fontWeight: 'bold', fontSize: 18}}>{following}</Text>
+                <Text>Following</Text>
+            </View>
         </View>
-        <View style={{alignItems: 'center'}}>
-          <Text style={{fontWeight: 'bold', fontSize: 18}}>{followers}</Text>
-          <Text>Followers</Text>
-        </View>
-        <View style={{alignItems: 'center'}}>
-          <Text style={{fontWeight: 'bold', fontSize: 18}}>{following}</Text>
-          <Text>Following</Text>
-        </View>
-      </View>
+        <Text>{bio}</Text>
+    </SafeAreaView>
+
   );
 };
