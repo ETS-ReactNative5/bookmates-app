e44f8e49c6f95c04d5c309e45e7a1babaaaea2b1
Laurena Fayad <laurenafayad@gmail.com>
2022-03-22 16:05:14 +0200

    Feed Screen: Added toggle functions for like/dislike

--- Frontend/components/ProfileReview.js
+++ Frontend/components/ProfileReview.js
@@ -1,26 +1,52 @@
-import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native'
-import React from 'react'
+import { SafeAreaView, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
 import { AntDesign, FontAwesome} from '@expo/vector-icons';
 
 const ProfileReview = ({user, book,review_text,likes,dislikes, comments}) => {
+
+  const [like_status, setLikeStatus] = useState(false);
+  const [dislike_status, setDislikeStatus] = useState(false);
+
+  const likeReview = () =>{
+    setLikeStatus(!like_status)
+  }
+
+  const dislikeReview = () =>{
+    setDislikeStatus(!dislike_status)
+  }
+
 
   return (
     <SafeAreaView>
       <View style={{flexDirection:'row', alignItems:'flex-start', backgroundColor:'white', marginVertical:5}}>
         <Image style={styles.profile_pic} source={user.profile_pic}/>
         <View>
-          <View style={styles.header}>
-            <Text style={styles.name}>{user.name}</Text>
-          </View>
+          <Text style={styles.name}>{user.name}</Text>
           <Text style={styles.book_title}>on {book.title} by {book.author}</Text>
           <View style={{flexDirection:'row'}}>
             <Image style={styles.book_img} source={book.thumbnail}/>
             <Text style={{fontFamily:'Roboto_300Light',flex: 1, flexWrap: 'wrap', paddingHorizontal:10}}>{review_text}</Text>
           </View>
-          <View style={{flexDirection:'row', justifyContent:'space-between', paddingHorizontal:10, paddingVertical:10}}>
-            <Text style={{color:"#6886C5"}}>{likes}<AntDesign name="like2" size={20} color="#6886C5"/></Text>
-            <Text style={{color:"#6886C5"}}>{dislikes}<AntDesign name="dislike2" size={20} color="#6886C5" /></Text>
-            <Text style={{color:"#6886C5"}}>{comments}<FontAwesome name="commenting-o" size={20} color="#6886C5" /></Text>
+          <View style={{flexDirection:'row', justifyContent:'space-between', paddingLeft:65, paddingVertical:10}}>
+            
+            <TouchableOpacity onPress={() => likeReview()}>
+              {like_status ? 
+              (<Text style={{color:"#6886C5"}}>{likes} <AntDesign name="like2" size={18} color="#6886C5"/></Text>)
+              :(<Text style={{color:"#6886C5"}}>{likes} <AntDesign name="like1" size={18} color="#6886C5" /></Text>)}
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => dislikeReview()}>
+              {dislike_status ? 
+              (<Text style={{color:"#6886C5"}}>{dislikes} <AntDesign name="dislike2" size={18} color="#6886C5" /></Text>)
+              :(<Text style={{color:"#6886C5"}}>{dislikes} <AntDesign name="dislike1" size={18} color="#6886C5" /></Text>)}
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => dislikeReview()}>
+              
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Text style={{color:"#6886C5"}}>{comments} <FontAwesome name="commenting-o" size={18} color="#6886C5" /></Text>
+            </TouchableOpacity>
           </View>
         </View>
       </View>
@@ -41,20 +67,19 @@ const styles = StyleSheet.create({
   name:{
     fontWeight:'bold',
     fontSize: 16,
-  },
-  book_title:{
-    fontStyle:"italic",
-    paddingHorizontal:10,
-    paddingVertical: 5,
-    fontFamily:'Roboto_300Light_Italic'
-  },
-  book_img:{
-    height: 67,
-    width: 45,
-    marginLeft:10,
-  },
-  header:{
     paddingHorizontal:10,
     paddingVertical: 5
-  }
+  },
+  book_title:{
+    paddingHorizontal:10,
+    paddingVertical: 5,
+    fontFamily:'Roboto_300Light_Italic',
+    fontSize:14
+  },
+  book_img:{
+    height: 85,
+    width: 65,
+    marginLeft:10,
+    borderRadius:6,
+  },
 })
