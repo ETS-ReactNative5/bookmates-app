88f055abb204f124540c34fdbf4d68e6e77fcef1
Laurena Fayad <laurenafayad@gmail.com>
2022-03-22 16:53:30 +0200

    Feed Screen: Fixed paddings, text justification and interactions icons positioning

--- Frontend/components/ProfileReview.js
+++ Frontend/components/ProfileReview.js
@@ -1,85 +1,114 @@
-import { SafeAreaView, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native'
-import React, { useState } from 'react'
-import { AntDesign, FontAwesome} from '@expo/vector-icons';
+import { SafeAreaView, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { AntDesign, FontAwesome } from '@expo/vector-icons';
 
-const ProfileReview = ({user, book,review_text,likes,dislikes, comments}) => {
-
+const ProfileReview = ({ user, book, review_text, likes, dislikes, comments }) => {
   const [like_status, setLikeStatus] = useState(false);
   const [dislike_status, setDislikeStatus] = useState(false);
 
-  const likeReview = () =>{
-    setLikeStatus(!like_status)
-  }
+  const likeReview = () => {
+    setLikeStatus(!like_status);
+  };
 
-  const dislikeReview = () =>{
-    setDislikeStatus(!dislike_status)
-  }
-
+  const dislikeReview = () => {
+    setDislikeStatus(!dislike_status);
+  };
 
   return (
     <SafeAreaView>
-      <View style={{flexDirection:'row', alignItems:'flex-start', backgroundColor:'white', marginVertical:5}}>
-        <Image style={styles.profile_pic} source={user.profile_pic}/>
-        <View>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'flex-start',
+          backgroundColor: 'white',
+          marginVertical: 5,
+          flex: 1,
+          paddingHorizontal: 15,
+        }}
+      >
+        <Image style={styles.profile_pic} source={user.profile_pic} />
+        <View style={{ flex: 1 }}>
           <Text style={styles.name}>{user.name}</Text>
-          <Text style={styles.book_title}>on {book.title} by {book.author}</Text>
-          <View style={{flexDirection:'row'}}>
-            <Image style={styles.book_img} source={book.thumbnail}/>
-            <Text style={{fontFamily:'Roboto_300Light',flex: 1, flexWrap: 'wrap', paddingHorizontal:10}}>{review_text}</Text>
+          <Text style={styles.book_title}>
+            on {book.title} by {book.author}
+          </Text>
+          <View style={{ flexDirection: 'row' }}>
+            <Image style={styles.book_img} source={book.thumbnail} />
+            <Text style={styles.review_text}>{review_text}</Text>
           </View>
-          <View style={{flexDirection:'row', justifyContent:'space-between', paddingLeft:65, paddingVertical:10}}>
-            
+
+          {/* Interactions */}
+          <View style={{ flexDirection: 'row', justifyContent: 'space-around', paddingVertical: 10 }}>
             <TouchableOpacity onPress={() => likeReview()}>
-              {like_status ? 
-              (<Text style={{color:"#6886C5"}}>{likes} <AntDesign name="like2" size={18} color="#6886C5"/></Text>)
-              :(<Text style={{color:"#6886C5"}}>{likes} <AntDesign name="like1" size={18} color="#6886C5" /></Text>)}
+              {like_status ? (
+                <Text style={{ color: '#6886C5' }}>
+                  {likes} <AntDesign name="like1" size={18} color="#6886C5" />
+                </Text>
+              ) : (
+                <Text style={{ color: '#6886C5' }}>
+                  {likes} <AntDesign name="like2" size={18} color="#6886C5" />
+                </Text>
+              )}
             </TouchableOpacity>
 
             <TouchableOpacity onPress={() => dislikeReview()}>
-              {dislike_status ? 
-              (<Text style={{color:"#6886C5"}}>{dislikes} <AntDesign name="dislike2" size={18} color="#6886C5" /></Text>)
-              :(<Text style={{color:"#6886C5"}}>{dislikes} <AntDesign name="dislike1" size={18} color="#6886C5" /></Text>)}
+              {dislike_status ? (
+                <Text style={{ color: '#6886C5' }}>
+                  {dislikes} <AntDesign name="dislike1" size={18} color="#6886C5" />
+                </Text>
+              ) : (
+                <Text style={{ color: '#6886C5' }}>
+                  {dislikes} <AntDesign name="dislike2" size={18} color="#6886C5" />
+                </Text>
+              )}
             </TouchableOpacity>
 
-            <TouchableOpacity onPress={() => dislikeReview()}>
-              
-            </TouchableOpacity>
             <TouchableOpacity>
-              <Text style={{color:"#6886C5"}}>{comments} <FontAwesome name="commenting-o" size={18} color="#6886C5" /></Text>
+              <Text style={{ color: '#6886C5' }}>
+                {comments} <FontAwesome name="commenting-o" size={18} color="#6886C5" />
+              </Text>
             </TouchableOpacity>
           </View>
         </View>
       </View>
     </SafeAreaView>
-  )
+  );
 };
 
 export default ProfileReview;
 
 const styles = StyleSheet.create({
-  profile_pic:{
+  profile_pic: {
     width: 50,
     height: 50,
-    borderRadius:100,
-    marginLeft:10,
-    marginTop:10
+    borderRadius: 100,
+    marginTop: 10,
   },
-  name:{
-    fontWeight:'bold',
+  name: {
+    fontWeight: 'bold',
     fontSize: 16,
-    paddingHorizontal:10,
-    paddingVertical: 5
+    paddingHorizontal: 10,
+    marginTop: 10,
   },
-  book_title:{
-    paddingHorizontal:10,
+  book_title: {
+    paddingHorizontal: 10,
     paddingVertical: 5,
-    fontFamily:'Roboto_300Light_Italic',
-    fontSize:14
+    fontFamily: 'Roboto_300Light_Italic',
+    fontSize: 14,
   },
-  book_img:{
+  book_img: {
     height: 85,
-    width: 65,
-    marginLeft:10,
-    borderRadius:6,
+    width: 60,
+    marginHorizontal: 10,
+    borderRadius: 6,
+    marginTop:4
   },
-})
+  review_text: {
+    textAlign: 'justify',
+    fontFamily: 'Roboto_300Light',
+    flex: 1,
+    flexWrap: 'wrap',
+    flexGrow: 1,
+    lineHeight: 18,
+  },
+});
--- Frontend/screens/Feed.js
+++ Frontend/screens/Feed.js
@@ -4,18 +4,23 @@ import ProfileReview from '../components/ProfileReview';
 
 const Feed = () => {
 
-  let book = {title:"Eleanor & Park", author:"Rainbow Rowell", 
+  let book1 = {title:"Me Before You", author:"Jojo Moyes", 
+  thumbnail:require('./../assets/mebeforeyou.jpg')};
+  let book2 = {title:"Eleanor & Park", author:"Rainbow Rowell", 
   thumbnail:require('./../assets/eandp.jpg')};
+  let book3 = {title:"To Kill A Mockingbird", author:"Harper Lee", 
+  thumbnail:require('./../assets/mockingbird.jpg')};
 
-  let user = {name:'Jane Dow', profile_pic:require('./../assets/test_profile_pic.jpg')};
+  let user1 = {name:'Jane Dow', profile_pic:require('./../assets/test_profile_pic.jpg')};
+  let user2 = {name:'Claudia Holland', profile_pic:require('./../assets/test_profile_pic.jpg')};
+  let user3 = {name:'Julia Collins', profile_pic:require('./../assets/test_profile_pic.jpg')};
   return (
     <SafeAreaView>
-      <ScrollView>
-
+      <ScrollView showsVerticalScrollIndicator={false}>
         <Text style={styles.title}>Reviews Feed</Text> 
         <ProfileReview
-          user = {user}
-          book = {book}
+          user = {user1}
+          book = {book1}
           review_text="I love it. I love its warmth and vibrancy, its heartache and its pain, its humor and meanness, the ugliness, the beauty, the crying, the laughter, the sarcasm.
           I love Elanor and Park and I love that there's still a tiny chance for them."
           likes="20"
@@ -23,8 +28,8 @@ const Feed = () => {
           comments="5"/>
 
           <ProfileReview
-          user = {user}
-          book = {book}
+          user = {user2}
+          book = {book2}
           review_text="I love it. I love its warmth and vibrancy, its heartache and its pain, its humor and meanness, the ugliness, the beauty, the crying, the laughter, the sarcasm.
           I love Elanor and Park and I love that there's still a tiny chance for them."
           likes="20"
@@ -32,8 +37,8 @@ const Feed = () => {
           comments="5"/>
 
           <ProfileReview
-          user = {user}
-          book = {book}
+          user = {user3}
+          book = {book3}
           review_text="I love it. I love its warmth and vibrancy, its heartache and its pain, its humor and meanness, the ugliness, the beauty, the crying, the laughter, the sarcasm.
           I love Elanor and Park and I love that there's still a tiny chance for them."
           likes="20"
