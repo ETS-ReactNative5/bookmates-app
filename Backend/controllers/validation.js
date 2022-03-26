a5078d29925295f2a711d8d224ee56f4fdf185ae
Laurena Fayad <laurenafayad@gmail.com>
2022-03-26 10:33:26 +0200

    Backend: Moved register validation logic to a separate controller

--- Backend/controllers/validation.js
+++ Backend/controllers/validation.js
@@ -1,8 +1,12 @@
+const User = require ('../models/User');
+const bcrypt = require ('bcryptjs');
+const jwt = require ('jsonwebtoken');
 //Validation using JOI
 const Joi = require('joi');
 
 //Registration Validation
-const registerValidation = data => {
+const registerValidation = async (req, res) => {
+
     const schema = Joi.object({
         first_name: Joi.string()
             .min(3)
@@ -18,7 +22,31 @@ const registerValidation = data => {
             .min(6),
     });
 
-    return schema.validate(data);
+    //Validate request data based on schema
+    const {error} = schema.validate(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    //Check if user is already registered
+    const emailMatch = await User.findOne({email: req.body.email});
+    if(emailMatch) return res.status(400).send('Email already exists');
+    
+    //Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    
+    //If data is validated, save user in db
+    const user = new User ({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        password: hashedPassword
+    });
+    try{
+        const savedUser = await user.save();
+        res.send(savedUser);
+    }catch(err){
+        res.status(400).send(err);
+    }    
 };
 
 //Login Validation
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,38 +1,8 @@
 const router = require ('express').Router();
-const User = require ('../models/User');
-const bcrypt = require ('bcryptjs');
-const jwt = require ('jsonwebtoken');
 const {registerValidation, loginValidation} = require('../controllers/validation');
 
 //Register
-router.post('/register', async (req, res) => {
-
-    //Validate request data based on schema
-    const {error} = registerValidation(req.body);
-    if(error) return res.status(400).send(error.details[0].message);
-
-    //Check if user is already registered
-    const emailMatch = await User.findOne({email: req.body.email});
-    if(emailMatch) return res.status(400).send('Email already exists');
-
-    //Hash password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(req.body.password, salt);
-
-    //If data is validated, save user in db
-    const user = new User ({
-        first_name: req.body.first_name,
-        last_name: req.body.last_name,
-        email: req.body.email,
-        password: hashedPassword
-    });
-    try{
-        const savedUser = await user.save();
-        res.send(savedUser);
-    }catch(err){
-        res.status(400).send(err);
-    }
-});
+router.post('/register', registerValidation);
 
 //Login
 router.post('/login', async (req, res) => {
