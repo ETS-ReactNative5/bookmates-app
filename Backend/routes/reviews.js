1c8b2a05df05aa36cf4c17c7177e372fd531117a
Laurena Fayad <laurenafayad@gmail.com>
2022-03-26 16:29:01 +0200

    Backend: Added EditReview api

--- Backend/routes/reviews.js
+++ Backend/routes/reviews.js
@@ -1,8 +1,9 @@
 const router = require ('express').Router();
-const { addReview } = require('../controllers/reviews');
+const { addReview, editReview } = require('../controllers/reviews');
 const verify = require ('./verifyToken');
 
 //verify will make sure that the user posting reviews is authorized (Protected route)
 router.post('/add', addReview);
+router.put('/edit/:id', editReview);
 
 module.exports = router;
