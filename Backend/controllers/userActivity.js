7c6f884a3b0324af88a5e7f67e53c8a1eaa7ee5a
Laurena Fayad <laurenafayad@gmail.com>
2022-03-26 12:57:47 +0200

    Backend: Added an api to follow a user

--- Backend/controllers/userActivity.js
+++ Backend/controllers/userActivity.js
@@ -1,8 +1,27 @@
 const User = require ('../models/User');
 
 const follow = async (req, res) => {
-    res.send('Followed')
-}
+
+    //Make sure the current user and user being followed are not the same
+    if (req.body.userId !== req.params.id) {
+        try {
+          const user = await User.findById(req.params.id);
+          const currentUser = await User.findById(req.body.userId);
+          //Make sure current user is trying to follow a new user and update lists accordingly
+          if (!user.followers.includes(req.body.userId)) {
+            await user.updateOne({ $push: { followers: req.body.userId } });
+            await currentUser.updateOne({ $push: { following: req.params.id } });
+            res.status(200).send("User successfully followed!");
+          } else {
+            res.status(403).send("You already follow this user!");
+          }
+        } catch (err) {
+          res.status(500).json(err);
+        }
+    } else {
+        res.status(403).send("You can't follow yourself.");
+    }}
+
 
 const unfollow = async (req, res) => {
     res.send('Unfollowed')
--- Backend/routes/userActivity.js
+++ Backend/routes/userActivity.js
@@ -1,8 +1,9 @@
 const router = require ('express').Router();
 const {follow, unfollow} = require('../controllers/userActivity');
+const verifyToken = require('./verifyToken');
 
 
-router.put('/follow', follow);
-router.put('/unfollow', unfollow);
+router.put('/follow/:id', verifyToken ,follow);
+router.put('/unfollow/:id', unfollow);
 
 module.exports = router;
