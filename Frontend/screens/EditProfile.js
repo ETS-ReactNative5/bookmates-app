dc1be03e7c54b911f33c90ee8d26fbf56bebe23c
Laurena Fayad <laurenafayad@gmail.com>
2022-03-29 17:29:33 +0300

    Edit Profile Screen: Linked to the edit profile api

--- Frontend/screens/EditProfile.js
+++ Frontend/screens/EditProfile.js
@@ -1,144 +1,185 @@
-import React from 'react';
-import {View,Text,TouchableOpacity,ToastAndroid,Image,TextInput,SafeAreaView} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import {View,Text,TouchableOpacity,ToastAndroid,Image,TextInput,SafeAreaView, ScrollView} from 'react-native';
 import Ionic from 'react-native-vector-icons/Ionicons';
+import axios from 'axios';
 
 const EditProfile = ({route, navigation}) => {
+
   const {first_name, last_name, profileImage, email, bio} = route.params;
 
-  return (
+  const [fname, setFName] = useState(first_name);
+  const [lname, setLName] = useState(last_name);
+  const [profileImageURL, setProfileImageURL] = useState(profileImage);
+  const [bioText, setBioText] = useState(bio);
+  const [emailAddress, setEmailAddress] = useState(email);
+
+  async function editData(){
+    try {
+        const { data } = await axios({
+            method: 'put',
+            headers:{
+              "Authorization":"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjQwMzYzOTFkOTA1ZTEwZTVmYzYwZDYiLCJpYXQiOjE2NDgzOTUwNjl9.L6bFuQ50tiGUFhfJrc-81CmVXVH1Xr-DmOXIj2-gvR0"
+            },
+            url: 'http://10.0.2.2:3000/api/user/editprofile',
+            data: {
+              first_name: fname,
+              last_name: lname,
+              email: emailAddress,
+              profile_bio: bioText,
+            }
+        });
+
+        navigation.goBack()    
+      } catch (err) {
+        if (err.response.status === 404) {
+            console.log('Resource could not be found!');
+        } else {
+            console.log(err.message);
+        }
+    }
+  }
+  
+   return (
     <SafeAreaView>
-      <View style={{
-        width: '100%',
-        height: '100%',
-        backgroundColor: 'white',
-        paddingTop:20,
-        paddingHorizontal:10
-        }}>
-        <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            padding: 10,
+      <ScrollView>
+
+        <View style={{
+          width: '100%',
+          height: '100%',
+          backgroundColor: 'white',
+          paddingTop:20,
+          paddingHorizontal:10
           }}>
-          <TouchableOpacity onPress={() => navigation.goBack()}>
-            <Ionic name="close-outline" style={{fontSize: 35}} />
-          </TouchableOpacity>
-          <Text style={{fontSize: 16, fontWeight: 'bold'}}>Profile Settings</Text>
-          <TouchableOpacity
-            onPress={() => {navigation.goBack();}}>
-            <Ionic name="checkmark" style={{fontSize: 35, color: '#3493D9'}} />
-          </TouchableOpacity>
-        </View>
-        <View style={{padding: 20, alignItems: 'center'}}>
-          <Image
-            source={{uri: `${profileImage}`}}
-            style={{width: 80, height: 80, borderRadius: 100}}
-          />
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              padding: 10,
+            }}>
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+              <Ionic name="close-outline" style={{fontSize: 35}} />
+            </TouchableOpacity>
+            <Text style={{fontSize: 16, fontWeight: 'bold'}}>Profile Settings</Text>
+            <TouchableOpacity
+              onPress={editData}>
+              <Ionic name="checkmark" style={{fontSize: 35, color: '#3493D9'}} />
+            </TouchableOpacity>
+          </View>
+          <View style={{padding: 20, alignItems: 'center'}}>
+            <Image
+              source={{uri: `${profileImageURL}`}}
+              style={{width: 80, height: 80, borderRadius: 100}}
+            />
+            <TouchableOpacity>
+              <Text
+                style={{
+                  color: '#3493D9',
+                  paddingTop:10,
+                  fontSize:16
+                }}>
+                Change Profile Picture
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={{paddingHorizontal: 20}}>
+          <View style={{paddingTop: 10}}>
+              <Text
+                style={{
+                  opacity: 0.5,
+                }}>
+                First name
+              </Text>
+              <TextInput
+                placeholder="First name"
+                defaultValue={fname}
+                style={{
+                  fontSize: 16,
+                  borderBottomWidth: 1,
+                  borderColor: '#CDCDCD',
+                }}
+                onChangeText={(e) => setFName(e)}
+              />
+            </View>
+            <View style={{paddingTop: 20}}>
+              <Text
+                style={{
+                  opacity: 0.5,
+                }}>
+                Last name
+              </Text>
+              <TextInput
+                placeholder="Last name"
+                defaultValue={lname}
+                style={{
+                  fontSize: 16,
+                  borderBottomWidth: 1,
+                  borderColor: '#CDCDCD',
+                }}
+                onChangeText={(e) => setLName(e)}
+              />
+            </View>
+            <View style={{paddingTop: 20}}>
+              <Text
+                style={{
+                  opacity: 0.5,
+                }}>
+                Email
+              </Text>
+              <TextInput
+                placeholder="Email"
+                defaultValue={emailAddress}
+                style={{
+                  fontSize: 16,
+                  borderBottomWidth: 1,
+                  borderColor: '#CDCDCD',
+                }}
+                onChangeText={(e) => setEmailAddress(e)}
+              />
+            </View>
+            <View style={{paddingTop: 20}}>
+            <Text
+                style={{
+                  opacity: 0.5,
+                }}>
+                Bio
+              </Text>
+              <TextInput
+                placeholder="Bio"
+                defaultValue={bioText}
+                multiline={true}
+                style={{
+                  fontSize: 16,
+                  borderBottomWidth: 1,
+                  borderColor: '#CDCDCD',
+                }}
+                onChangeText={(e) => setBioText(e)}
+              />
+            </View>
+          </View>
           <TouchableOpacity>
             <Text
-              style={{
-                color: '#3493D9',
-                paddingTop:10,
-                fontSize:16
-              }}>
-              Change Profile Picture
-            </Text>
+                style={{
+                  color: '#3493D9',
+                  paddingTop:30,
+                  paddingHorizontal:20,
+                  fontSize:16
+                }}>
+                Change Password
+              </Text>
           </TouchableOpacity>
+            <TouchableOpacity>
+              <Text
+                style={{
+                  color: '#3493D9',
+                  padding:20,
+                  fontSize:16
+                }}>
+                Sign out
+              </Text>
+            </TouchableOpacity>
         </View>
-        <View style={{paddingHorizontal: 20}}>
-        <View style={{paddingTop: 10}}>
-            <Text
-              style={{
-                opacity: 0.5,
-              }}>
-              First name
-            </Text>
-            <TextInput
-              placeholder="First name"
-              defaultValue={first_name}
-              style={{
-                fontSize: 16,
-                borderBottomWidth: 1,
-                borderColor: '#CDCDCD',
-              }}
-            />
-          </View>
-          <View style={{paddingTop: 20}}>
-            <Text
-              style={{
-                opacity: 0.5,
-              }}>
-              Last name
-            </Text>
-            <TextInput
-              placeholder="Last name"
-              defaultValue={last_name}
-              style={{
-                fontSize: 16,
-                borderBottomWidth: 1,
-                borderColor: '#CDCDCD',
-              }}
-            />
-          </View>
-          <View style={{paddingTop: 20}}>
-            <Text
-              style={{
-                opacity: 0.5,
-              }}>
-              Email
-            </Text>
-            <TextInput
-              placeholder="Email"
-              defaultValue={email}
-              style={{
-                fontSize: 16,
-                borderBottomWidth: 1,
-                borderColor: '#CDCDCD',
-              }}
-            />
-          </View>
-          <View style={{paddingTop: 20}}>
-          <Text
-              style={{
-                opacity: 0.5,
-              }}>
-              Bio
-            </Text>
-            <TextInput
-              placeholder="Bio"
-              defaultValue={bio}
-              multiline={true}
-              style={{
-                fontSize: 16,
-                borderBottomWidth: 1,
-                borderColor: '#CDCDCD',
-              }}
-            />
-          </View>
-        </View>
-        <TouchableOpacity>
-          <Text
-              style={{
-                color: '#3493D9',
-                paddingTop:30,
-                paddingHorizontal:20,
-                fontSize:16
-              }}>
-              Change Password
-            </Text>
-        </TouchableOpacity>
-          <TouchableOpacity>
-            <Text
-              style={{
-                color: '#3493D9',
-                padding:20,
-                fontSize:16
-              }}>
-              Sign out
-            </Text>
-          </TouchableOpacity>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
