f3f7c02b80818136815f2630d586b87d02ae3e53
Laurena Fayad <laurenafayad@gmail.com>
2022-04-02 01:08:12 +0300

    Search Screen: Search bar linked to search book api and basic search result design done

--- Frontend/screens/Search.js
+++ Frontend/screens/Search.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import {
   SafeAreaView,
   RefreshControl,
+  Modal,
   View,
   Text,
   Image,
@@ -10,6 +11,7 @@ import {
   ScrollView,
   FlatList,
   StatusBar,
+  StyleSheet
 } from 'react-native';
 import * as SecureStore from 'expo-secure-store';
 import { Searchbar } from 'react-native-paper';
@@ -18,7 +20,9 @@ import BookSearch from '../components/BookSearch';
 const Search = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [suggestions, setSuggestions] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
   const [refreshing, setRefreshing] = useState(true);
+  const [modalVisible, setModalVisible] = useState(false);
 
   const onChangeSearch = (query) => {
     setSearchQuery(query);
@@ -27,6 +31,25 @@ const Search = () => {
   useEffect(() => {
     getSuggestions();
   }, []);
+
+  const getSearchResults = async () => {
+    if(searchQuery){
+      const token = await SecureStore.getItemAsync('token')
+      await fetch('http://192.168.1.10:3000/api/book/search/' + searchQuery, {
+        headers: {
+          Authorization:
+            'Bearer '+token,
+        },
+      })
+        .then((res) => res.json())
+        .then((result) =>{
+          setSearchResults(result)
+          setRefreshing(false);
+          setModalVisible(true)
+        })
+        .catch((err) => console.log(err));
+    }
+  }
 
   const getSuggestions = async () => {
     const token = await SecureStore.getItemAsync('token')
@@ -55,6 +78,7 @@ const Search = () => {
         placeholder="Search"
         onChangeText={onChangeSearch}
         value={searchQuery}
+        onSubmitEditing={getSearchResults}
         style={{
           height: 40,
           alignSelf: 'center',
@@ -70,11 +94,10 @@ const Search = () => {
         Wondering what to read next?
       </Text>
       <Text
-        style={{ color: 'black', paddingLeft: 25, fontFamily: 'Roboto_300Light_Italic', fontSize: 14, marginBottom: 8 }}
+        style={{ color: 'black', paddingLeft: 25, fontFamily: 'Roboto_300Light_Italic', fontSize: 14, marginBottom: 12 }}
       >
         Search a library of over 10 million books!
       </Text>
-      <Text>{searchQuery}</Text>
 
 
       <FlatList
@@ -86,8 +109,85 @@ const Search = () => {
         numColumns={3}
         showsVerticalScrollIndicator={false}
       />
+
+      <Modal
+        animationType="fade"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+            <View style={styles.modalContainerStyle}>
+            <View
+              style={{
+                borderRadius: 20,
+                padding: 20,
+                width: 300,
+                alignItems: 'flex-start',
+                justifyContent: 'center',
+                flexDirection: 'column',
+                backgroundColor: '#FFFFFFFF',
+              }}
+            >
+              <Text style={styles.searchHeader}>Search Results</Text>
+              <ScrollView showsHorizontalScrollIndicator={false} showsVerticalScrollIndicator={false}>
+
+              <View style={{ justifyContent: 'center', width:'100%' }}>
+                {searchResults.map((result) => {
+                  return(
+                    <View style={{flexDirection: 'row'}}>
+                      <Image source={{uri: `${result?.thumbnail}`}} style={styles.thumbnail}/>
+                      <View>
+                        <Text style={{marginTop:5}}>{result.title}</Text>
+                        <Text style={{fontFamily: 'Roboto_300Light_Italic', marginTop:2}}>By {result.author}</Text>                      
+                      </View>
+                    </View>
+                  )})}
+
+          </View>
+              </ScrollView>
+        </View>
+          </View>
+                {/* <TouchableOpacity
+                  style={styles.button}
+                  onPress={() => {
+                    navigation.navigate('BookmateProfile', { user: selectedBookmate });
+                    setModalVisible(!modalVisible);
+                  }}
+                >
+                </TouchableOpacity> */}
+      </Modal>
+
     </SafeAreaView>
   );
 };
 
 export default Search;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  modalContainerStyle: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+  },
+  searchHeader: {
+    fontFamily: "Baloo2_800ExtraBold",
+    color: "#5A7FCC",
+    fontSize:20,
+    marginBottom:15,
+  },
+  thumbnail: {
+    height: 120,
+    width: 80,
+    marginVertical:5,
+    marginHorizontal:5,
+    borderRadius:10,
+    marginRight: 10
+  }
+});
