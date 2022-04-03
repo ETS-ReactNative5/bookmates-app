46add84a77182401ea1c2b5a3c777ab70f26b8e0
Laurena Fayad <laurenafayad@gmail.com>
2022-04-03 15:43:02 +0300

    Backend: Added notifications to User schema

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -41,7 +41,15 @@ const userSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
   }],
-
+  notifications: [{
+    from: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    action: {
+      type: String
+    }
+  }],
   finishedBooks: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Book'
